c061c7b728d937ee4d978633022ae3d6e0cfbc2f
rebleo <rml444@nyu.edu>
2024-04-16 19:36:19 -0400

    p5 example

--- week11/code/potato.js
+++ week11/code/potato.js
@@ -1,1 +1,17 @@
 console.log("potato!")
+
+let theWhy = 600;
+
+function setup() {
+    //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+  //60 frames per seccond
+  // the canvas is red
+  background(220, 0,0);
+  ellipse(100,theWhy--, 50, 10);
+  rect(mouseX,mouseY, 100)
+}
